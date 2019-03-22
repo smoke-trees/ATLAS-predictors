f510b3e57c77de42aaa43a02bf86aea537baf2c6
Tanmay Thakur <thakurtanmay72@yahoo.com>
2019-03-23 02:34:21 +0530

    Better Models,Vid Models

---
+++ hum_detector_alt.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Mar 23 01:37:56 2019
+
+@author: tanma
+"""
+
+from imageai.Detection import ObjectDetection
+import os
+
+execution_path = os.getcwd()
+
+detector = ObjectDetection()
+#detector.setModelTypeAsRetinaNet()
+#detector.setModelPath( os.path.join(execution_path , "resnet50_coco_best_v2.0.1.h5"))
+detector.setModelTypeAsYOLOv3()
+detector.setModelPath( os.path.join(execution_path , "yolo.h5"))
+detector.loadModel()
+detections = detector.detectObjectsFromImage(input_image=os.path.join(execution_path , "image.jpeg"), output_image_path=os.path.join(execution_path , "imagenew.jpg"))
+
+for eachObject in detections:
+    print(eachObject["name"] , " : " , eachObject["percentage_probability"] )
---
+++ hum_detector_self.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Mar 23 02:17:08 2019
+
+@author: tanma
+"""
+
+from imageai.Detection import VideoObjectDetection
+import os
+import cv2
+
+execution_path = os.getcwd()
+
+camera = cv2.VideoCapture(0) 
+
+detector = VideoObjectDetection()
+detector.setModelTypeAsYOLOv3()
+detector.setModelPath(os.path.join(execution_path , "yolo.h5"))
+detector.loadModel()
+
+video_path = detector.detectObjectsFromVideo(camera_input=camera,
+                                output_file_path=os.path.join(execution_path, "camera_detected_1")
+                                , frames_per_second=29, log_progress=True)
+print(video_path)
